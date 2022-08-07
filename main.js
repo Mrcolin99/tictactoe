f0dfb10c5fc55e3661930ef0277a6c1f80b73a3c
Colin Ciervo <colinciervo@@gmail.com>
2022-08-07 11:42:22 -0400

    Add assets folder

---
+++ main.js
@@ -0,0 +1,18 @@
+//Query
+var ttt1 = document.querySelector('.ttt1')
+var ttt2 = document.querySelector('.ttt2')
+var ttt3 = document.querySelector('.ttt3')
+var ttt4 = document.querySelector('.ttt4')
+var ttt5 = document.querySelector('.ttt5')
+var ttt6 = document.querySelector('.ttt6')
+var ttt7 = document.querySelector('.ttt7')
+var ttt8 = document.querySelector('.ttt8')
+var ttt9 = document.querySelector('.ttt9')
+
+
+
+//Event Listeners
+
+
+
+//Functions
