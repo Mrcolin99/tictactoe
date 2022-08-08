bf75bc43dd590a54e1774e4d3f307616e1d5b3a7
Colin Ciervo <colinciervo@@gmail.com>
2022-08-08 18:50:09 -0400

    Game class exists

---
+++ game.js
@@ -0,0 +1,9 @@
+class Game {
+    constructor(player1, player2) {
+        this.board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        this.player1Turn = true
+        this.player1 = player1
+        this.player2 = player2
+    }
+    
+}
