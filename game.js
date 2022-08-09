04a2108f7e192fe310c93f8cf6d8acd073a05ef7
Colin Ciervo <colinciervo@@gmail.com>
2022-08-09 00:13:20 -0400

    Func: game board added

--- game.js
+++ game.js
@@ -4,6 +4,54 @@ class Game {
         this.player1Turn = true
         this.player1 = player1
         this.player2 = player2
+        this.turn = 0
     }
-    
+    changePlayer() {
+        if (this.player1Turn === true) {
+            this.player1Turn = false
+        } else { this.player1Turn = true }
+    }
+    makeMove(event) {
+        var move = event.target.attributes.sector.value
+        if (this.player1Turn === true) {
+            this.board[move] = 1
+        } else { this.board[move] = 2 }
+        this.turn++
+        console.log(this.board)
+        console.log(this.turn)
+    }
+    winCondition() {
+        var winBoard = [
+            [1, 1, 1, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 1, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 1, 1, 1],
+            [1, 0, 0, 1, 0, 0, 1, 0, 0],
+            [0, 1, 0, 0, 1, 0, 0, 1, 0],
+            [0, 0, 1, 0, 0, 1, 0, 0, 1],
+            [1, 0, 0, 0, 1, 0, 0, 0, 1],
+            [0, 0, 1, 0, 1, 0, 1, 0, 0]
+        ]
+        for (var i = 0; i < winBoard.length; i++) {
+            if (this.board.includes(winBoard[i])) {
+                this.playerWin()
+            } else if (this.turn >= 9) {
+                this.drawGame()
+            }
+        }
+    }
+    drawGame() {
+        gameMsg.innerText = 'DRAW!'
+        this.resetGame()
+    }
+
+    resetGame() {
+        this.turn = 0
+        this.player1Turn = true
+        this.board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    }
+
+    playerWin() {
+        gameMsg.innerText = 'PLAYER WIN!'
+        this.resetGame()
+    }
 }
--- main.js
+++ main.js
@@ -1,7 +1,17 @@
+//var
+var player1 = new Player('Greg')
+var player2 = new Player('Ted')
+var game = new Game(player1, player2)
 //Query
-
+var gameBoard = document.querySelector('.sec3')
+var gameMsg = document.querySelector('.game-msg')
 //Arrays
 
 //Event Listeners
-
+gameBoard.addEventListener('click', clickHandler)
 //Functions
+function clickHandler() {
+    game.makeMove(event)
+    game.changePlayer()
+    game.winCondition()
+}
