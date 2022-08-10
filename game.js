a5ea51a859469fc68b8379086de924d79a608c2c
Colin Ciervo <colinciervo@@gmail.com>
2022-08-10 12:32:53 -0400

    Smybol and clear working

--- game.js
+++ game.js
@@ -19,13 +19,13 @@ class Game {
     }
     makeMove(event) {
         var move = parseInt(event.target.attributes.sector.value)
-        if (this.player1Turn === true && !player2.moves.includes(move)) {
+        if (this.player1Turn === true && !player2.moves.includes(move) && !player1.moves.includes(move)) {
             player1.moves.push(move)
-            // markSector(event)
+            markSector(player1)
             console.log(player1.moves)
-        } else if (this.player1Turn === false && !this.player1.moves.includes(move)) {
+        } else if (this.player1Turn === false && !this.player1.moves.includes(move) && !player2.moves.includes(move)) {
             player2.moves.push(move)
-            // markSector(event)
+            markSector(player2)
             console.log(player2.moves)
         } else { return }
         this.turn++
@@ -45,6 +45,14 @@ class Game {
         player1.moves = []
         player2.moves = []
         gameMsg.innerText = `${player1.name}s turn`
-        console.log(this.turn)
+        gameBoard.innerHTML = `<div class="ttt" id="1" sector="1"></div>
+        <div class="ttt" id="2" sector="2"></div>
+        <div class="ttt" id="3" sector="3"></div>
+        <div class="ttt" id="4" sector="4"></div>
+        <div class="ttt" id="5" sector="5"></div>
+        <div class="ttt" id="6" sector="6"></div>
+        <div class="ttt" id="7" sector="7"></div>
+        <div class="ttt" id="8" sector="8"></div>
+        <div class="ttt" id="9" sector="9"></div>`
     }
 }
--- main.js
+++ main.js
@@ -1,27 +1,36 @@
 //var
-var player1 = new Player('Greg')
-var player2 = new Player('Ted')
+var player1 = new Player('Rick')
+var player2 = new Player('Greg')
 var game = new Game(player1, player2)
 //Query
+var p1wincounter = document.querySelector('.p1')
+var p2wincounter = document.querySelector('.p2')
 var gameBoard = document.querySelector('.sec3')
 var gameMsg = document.querySelector('.game-msg')
 var p1Wins = document.querySelector('.p1Wins')
 var p2Wins = document.querySelector('.p2Wins')
 var startNewGame = document.querySelector('.newgame')
 var overlay = document.querySelector('.overlay')
-var ttt = document.querySelector('.ttt')
+var ttt = document.querySelectorAll('.ttt')
 //Event Listeners
-gameBoard.addEventListener('click', clickHandler)
-startNewGame.addEventListener('click', newGame)
 window.addEventListener('load', newGame)
+gameBoard.addEventListener('click', clickHandler)
+startNewGame.addEventListener('click', newGameButton)
 //Functions
 function clickHandler() {
     game.makeMove(event)
     game.drawGame()
 }
 
-function newGame() {
-    game.resetGame(player1, player2)
+function newGameButton() {
+    game.turn = 9
+    game.drawGame(player1, player2)
+}
+
+function newGame(player1, player2) {
+    // p1wincounter.innerText = `${player1.name} Wins:`
+    // p2wincounter.innerText = `${player2.name} Wins:`
+    game.drawGame(player1, player2)
 }
 
 function suspend(player1, player2) {
@@ -41,9 +50,9 @@ function updateScore(player1, player2) {
     p2Wins.innerText = `${player2.wins}`
 }
 
-function markSector(event) {
-    var currentSector = event.target.attributes.sector.value
-    if (game.player1Turn === true) {
-        ttt.currentSector.innerText = 'X'
-    } else {ttt.currentSector.innerText = 'O'}
+function markSector(player) {
+    var mark = game.player1Turn ? 'X' : 'O'
+    for (var i = 0; i < player.moves.length; i++) {
+        ttt[player.moves[i] - 1].innerText = mark
+    }
 }
